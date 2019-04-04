ccf50c4e2fb2a01e41aadc82939c2333e3dd673d
cs.mccaleb@gmail.com <cs.mccaleb@gmail.com>
2019-04-04 00:08:30 -0500

    Update poseidon.ino

--- poseidon/poseidon.ino
+++ poseidon/poseidon.ino
@@ -1,7 +1,17 @@
-void setup() {
+// Information on string conversion https://docs.particle.io/reference/device-os/firmware/photon/#string-class
+// MiCS-5524 senso
 
+String analogvalue;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("MiCS-5524 is running!");
+  Particle.variable("analogvalue", analogvalue);
 }
 
 void loop() {
-
+  String analogvalue = String(analogRead(A0));
+  Particle.publish("analogvalue", analogvalue);
+  // Wait for 30 seconds
+  delay(30000);               
 }
